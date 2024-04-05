7787a1fb20d6f92056c1f7da191d5acb800037ab
VitalikGrinya <vitalik240899@ya.ru>
2024-04-05 13:39:44 +0300

    Update MusicSettings.cs

--- Assets/Scripts/MusicSettings.cs
+++ Assets/Scripts/MusicSettings.cs
@@ -7,26 +7,30 @@ public class MusicSettings : MonoBehaviour
 {
     public AudioMixerGroup Mixer;
 
+    private float _minVolumeValue = -80;
+    private float _maxVolumeValue = 0;
+
+
     public void ToggleMusic(bool isPlaying)
     {
         if (isPlaying)
-            Mixer.audioMixer.SetFloat("BackgroundVolume", -80);
+            Mixer.audioMixer.SetFloat("BackgroundVolume", _minVolumeValue);
         else
-            Mixer.audioMixer.SetFloat("BackgroundVolume", 0);
+            Mixer.audioMixer.SetFloat("BackgroundVolume", _maxVolumeValue);
     }
 
     public void ChangeMasterVolume(float volume)
     {
-        Mixer.audioMixer.SetFloat("MasterVolume", Mathf.Lerp(-80, 0, volume));
+        Mixer.audioMixer.SetFloat("MasterVolume", Mathf.Lerp(_minVolumeValue, _maxVolumeValue, volume));
     }
 
     public void ChangeMelodyVolume(float volume)
     {
-        Mixer.audioMixer.SetFloat("MusicVolume", Mathf.Lerp(-80, 0, volume));
+        Mixer.audioMixer.SetFloat("MusicVolume", Mathf.Lerp(_minVolumeValue, _maxVolumeValue, volume));
     }
 
     public void ChangeBackgroundVolume(float volume)
     {
-        Mixer.audioMixer.SetFloat("BackgroundVolume", Mathf.Lerp(-80, 0, volume));
+        Mixer.audioMixer.SetFloat("BackgroundVolume", Mathf.Lerp(_minVolumeValue, _maxVolumeValue, volume));
     }
 }
