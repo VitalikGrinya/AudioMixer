7fbe837e0300c5b7fcb3661c9427433fa6ce3d7c
VitalikGrinya <vitalik240899@ya.ru>
2024-04-05 09:13:26 +0300

    Update Script

--- Assets/Scripts/Music.cs
+++ Assets/Scripts/Music.cs
@@ -1,4 +1,3 @@
-using UnityEngine.UI;
 using UnityEngine;
 
 public class Music : MonoBehaviour
@@ -6,14 +5,14 @@ public class Music : MonoBehaviour
     public bool _isPlaying = false;
     public AudioSource _audio;
 
-    public void SettingMusic()
+    public void Playing()
     {
         if(_isPlaying == false)
         {
             _audio.Play();
             _isPlaying = true;
         }
-        else if(_isPlaying == true)
+        else
         {
             _audio.Stop();
             _isPlaying = false;
